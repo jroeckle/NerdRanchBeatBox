b961f4c03d48ce930965824706baab5bd5526926
Jason Roeckle <jason.j.roeckle@disney.com>
2016-02-29 14:03:40 -0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -0,0 +1,47 @@
+package com.bignerdranch.android.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by roecj001 on 2/28/16.
+ */
+public class BeatBox {
+    private static final String TAG = "BeatBox";
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+
+    private AssetManager mAssets;
+    private List<Sound> mSounds = new ArrayList<>();
+
+    public BeatBox(Context context) {
+        mAssets = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds() {
+        String[] soundsNames;
+
+        try {
+            soundsNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundsNames.length + " sounds");
+        } catch (IOException ioe) {
+            Log.e(TAG, "Could not list assets", ioe);
+            return;
+        }
+
+        for (String filename : soundsNames) {
+            String assetPath = SOUNDS_FOLDER + "/" + filename;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
+        }
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
+    }
+}
